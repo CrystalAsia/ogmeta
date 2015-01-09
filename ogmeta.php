fa3f78d21851bad1ab5c98a2bbf13477a0b9f668
Ryan Snowden <ryan@crystal-asia.com>
2015-01-09 13:17:47 +0800

    Better tag support
    Description + truncate, adding in prefix="og: http://ogp.me/ns#"  because LinkedIn has terrible support

--- ogmeta.php
+++ ogmeta.php
@@ -14,29 +14,29 @@ class plgContentOgMeta extends JPlugin
     public function onContentBeforeDisplay($context, &$article, &$params)
     {
         global $mainframe;
-        if (JRequest::getVar('view') != 'featured') {
+        if (JRequest::getVar('view') == 'article') {
             $document = JFactory::getDocument();
 
             $articleTitle = JTable::getInstance("content");
             $articleTitle->load(JRequest::getInt("id"));
+            $introText = substr(strip_tags($articleTitle->get("introtext")), 0,250) . "...";
 
             $config = JFactory::getConfig();
             $u = JFactory::getURI();
 
-            $document->addCustomTag('<meta property="og:title" content="' . $articleTitle->get("title") . '" />');
-            $document->addCustomTag('<meta property="og:url" content="' . $u->toString() . '" />');
-            $document->addCustomTag('<meta property="og:site_name" content="' . $config->get('sitename') . '" />');
+            $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:title" content="' . $articleTitle->get("title") . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:url" content="' . $u->toString() . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:description" content="' . $introText . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="' . $config->get('sitename') . '" />');
+
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="article:publisher" content="' . JURI::root() . '" />');
 
             $ogImage = $this->params->get('ogimage');
             if ($ogImage != '') {
-                $document->addCustomTag('<meta property="og:image" content="' . $ogImage . '" />');
+                $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:image" content="' . $ogImage . '" />');
             }
 
-            if ($ogDesc != '') {
-                $document->addCustomTag('<meta property="og:description" content="' . $ogDesc . '" />');
-            }
-
-            $document->addCustomTag('<meta property="og:type" content="article" />');
+            $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:type" content="article" />');
         }
     }
 }

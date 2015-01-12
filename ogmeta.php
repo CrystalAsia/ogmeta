e5ac555d4ad9359555165ce219f7a210348e484f
Ryan Snowden <ryan@crystal-asia.com>
2015-01-12 11:02:16 +0800

    using $article object like it should be used

--- ogmeta.php
+++ ogmeta.php
@@ -6,37 +6,40 @@ jimport('joomla.plugin.plugin');
 class plgContentOgMeta extends JPlugin
 {
 
-    public function plgContentOgMeta(&$subject, $params)
-    {
-        parent::__construct($subject, $params);
-    }
+	public function plgContentOgMeta(&$subject, $params)
+	{
+		parent::__construct($subject, $params);
+	}
 
-    public function onContentBeforeDisplay($context, &$article, &$params)
-    {
-        global $mainframe;
-        if (JRequest::getVar('view') == 'article') {
-            $document = JFactory::getDocument();
+	public function onContentBeforeDisplay($context, &$article, &$params)
+	{
+		global $mainframe;
+		if (JRequest::getVar('view') == 'article') {
+			$document = JFactory::getDocument();
 
-            $articleTitle = JTable::getInstance("content");
-            $articleTitle->load(JRequest::getInt("id"));
-            $introText = substr(strip_tags($articleTitle->get("introtext")), 0,250) . "...";
+            // Sanitize description
+			$introText = substr(strip_tags($article->introtext), 0,250) . "...";
 
-            $config = JFactory::getConfig();
-            $u = JFactory::getURI();
+			// Remove quotes
+			$articleTitle = str_replace(array('"',"'"), "", $article->title);
+			$introText = str_replace(array('"',"'"), "", $introText);
 
-            $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:title" content="' . $articleTitle->get("title") . '" />');
-	    $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:url" content="' . $u->toString() . '" />');
-	    $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:description" content="' . $introText . '" />');
-	    $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="' . $config->get('sitename') . '" />');
+			$config = JFactory::getConfig();
+			$u = JFactory::getURI();
 
-	    $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="article:publisher" content="' . JURI::root() . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:title" content="' . $articleTitle . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:url" content="' . $u->toString() . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:description" content="' . $introText . '" />');
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="' . $config->get('sitename') . '" />');
 
-            $ogImage = $this->params->get('ogimage');
-            if ($ogImage != '') {
-                $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:image" content="' . $ogImage . '" />');
-            }
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="article:publisher" content="' . JURI::root() . '" />');
 
-            $document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:type" content="article" />');
-        }
-    }
+			$ogImage = $this->params->get('ogimage');
+			if ($ogImage != '') {
+				$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:image" content="' . $ogImage . '" />');
+			}
+
+			$document->addCustomTag('<meta prefix="og: http://ogp.me/ns#" property="og:type" content="article" />');
+		}
+	}
 }
